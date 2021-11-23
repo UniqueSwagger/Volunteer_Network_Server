af658dcaed447d53ac32740bebd7336f288d8100
UniqueSwagger <rahmanmahi02@gmail.com>
2021-11-23 12:56:16 +0600

    url-encoded

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.get("/", (req, res) => {
   res.send("Hello volunteer network");
 });
